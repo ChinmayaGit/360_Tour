82f34483532380a2c1011f742b1559d0c3b59031
CHINMAYA GARNAIK <chinugarnaiklabs@gmail.com>
2023-03-13 22:21:33 +0530

    fix

--- main.js
+++ main.js
@@ -40,6 +40,7 @@ progressElement = document.getElementById("progress");
 progressContainer = document.getElementById("progress_container");
 
 function onEnter(event) {
+
   progressContainer.classList.remove("hide");
   progressContainer.classList.add("show");
   progressElement.style.width = 0;
@@ -49,6 +50,7 @@ function onEnter(event) {
 }
 
 function onProgress(event) {
+  onEnter("enter");
   progress = (event.progress.loaded / event.progress.total) * 100;
   progressElement.style.width = progress + "%";
   if (progress === 100) {
@@ -58,55 +60,58 @@ function onProgress(event) {
       progressContainer.classList.add("hide");
     }, 500);
   }
+
+  console.log(progress);
+
 }
 
 container = document.querySelector(".image-container");
 
 panorama1 = new PANOLENS.ImagePanorama("images/4.jpg");
 panorama1.addEventListener("progress", onProgress);
-panorama1.addEventListener("enter", onEnter);
+// panorama1.addEventListener("enter", onEnter);
 panorama1.addEventListener("enter-fade-start", function () {
   viewer.tweenControlCenter(lookAtPositions[0], 0);
 });
 
 panorama2 = new PANOLENS.ImagePanorama("images/5.jpg");
 panorama2.addEventListener("progress", onProgress);
-panorama2.addEventListener("enter", onEnter);
+// panorama2.addEventListener("enter", onEnter);
 panorama2.addEventListener("enter", function () {
   viewer.tweenControlCenter(lookAtPositions[0], 0);
 });
 
 panorama3 = new PANOLENS.ImagePanorama("images/6.jpg");
 panorama3.addEventListener("progress", onProgress);
-panorama3.addEventListener("enter", onEnter);
+// panorama3.addEventListener("enter", onEnter);
 panorama3.addEventListener("enter", function () {
   viewer.tweenControlCenter(lookAtPositions[0], 0);
 });
 
 panorama1.link(panorama2, infospotPositions[0]);
 panorama2.link(panorama3, infospotPositions[0]);
-panorama3.link(panorama1, infospotPositions[1]);
+panorama3.link(panorama1, infospotPositions[0]);
 
-infospot = new PANOLENS.Infospot(350, PANOLENS.DataImage.Info);
-infospot.position.set(2000, 0000, -6000);
-infospot.addHoverText("Blazer", 30);
+// infospot = new PANOLENS.Infospot(350, PANOLENS.DataImage.Info);
+// infospot.position.set(2000, 0000, -6000);
+// infospot.addHoverText("Blazer", 30);
 
-infospotTwo = new PANOLENS.Infospot(350, PANOLENS.DataImage.Info);
-infospotTwo.position.set(2500, -3000, -6000);
-infospotTwo.addHoverText("Bucket", 30);
+// infospotTwo = new PANOLENS.Infospot(350, PANOLENS.DataImage.Info);
+// infospotTwo.position.set(2500, -3000, -6000);
+// infospotTwo.addHoverText("Bucket", 30);
 
-infospotThree = new PANOLENS.Infospot(350, PANOLENS.DataImage.Info);
-infospotThree.position.set(7000, -2000, 3700);
-infospotThree.addHoverText("Broken Socket", 30);
+// infospotThree = new PANOLENS.Infospot(350, PANOLENS.DataImage.Info);
+// infospotThree.position.set(7000, -2000, 3700);
+// infospotThree.addHoverText("Broken Socket", 30);
 
-infospotFour = new PANOLENS.Infospot(350, PANOLENS.DataImage.Info);
-infospotFour.position.set(5500, 0000, -0000);
-infospotFour.addHoverText("Broken Socket", 30);
+// infospotFour = new PANOLENS.Infospot(350, PANOLENS.DataImage.Info);
+// infospotFour.position.set(5500, 0000, -0000);
+// infospotFour.addHoverText("Broken Socket", 30);
 
-panorama1.add(infospot);
-panorama1.add(infospotTwo);
-panorama1.add(infospotFour);
-panorama2.add(infospotThree);
+// panorama1.add(infospot);
+// panorama1.add(infospotTwo);
+// panorama1.add(infospotFour);
+// panorama2.add(infospotThree);
 
 viewer = new PANOLENS.Viewer({
   output: "console",
@@ -115,62 +120,62 @@ viewer = new PANOLENS.Viewer({
 });
 viewer.add(panorama1, panorama2, panorama3);
 
-// Cube custom item
-var cube = new THREE.Mesh(
-  new THREE.BoxGeometry(100, 100, 100),
-  new THREE.MeshNormalMaterial()
-);
-cube.position.set(-300, 100, 100);
-viewer.addUpdateCallback(function () {
-  cube.rotation.y += 0.05;
-  cube.rotation.x += 0.02;
-});
-panorama1.add(cube);
+// // Cube custom item
+// var cube = new THREE.Mesh(
+//   new THREE.BoxGeometry(100, 100, 100),
+//   new THREE.MeshNormalMaterial()
+// );
+// cube.position.set(-300, 100, 100);
+// viewer.addUpdateCallback(function () {
+//   cube.rotation.y += 0.05;
+//   cube.rotation.x += 0.02;
+// });
+// panorama1.add(cube);
 
-var controlItemCube = {
-  style: {
-    backgroundImage: "url(http://i40.tinypic.com/1531w79.jpg)",
-  },
+// var controlItemCube = {
+//   style: {
+//     backgroundImage: "url(http://i40.tinypic.com/1531w79.jpg)",
+//   },
 
-  onTap: function () {
-    viewer.tweenControlCenterByObject(cube);
-  },
-};
-viewer.appendControlItem(controlItemCube);
+//   onTap: function () {
+//     viewer.tweenControlCenterByObject(cube);
+//   },
+// };
+// viewer.appendControlItem(controlItemCube);
 
-// Wonder women custom item
-var posterInfospot = new PANOLENS.Infospot(
-  2000,
-  "https://images-na.ssl-images-amazon.com/images/I/91nELBuo3kL._RI_SX200_.jpg"
-);
-posterInfospot.position.set(-4774.9, 474.16, -1375.02);
-panorama1.add(posterInfospot);
+// // Wonder women custom item
+// var posterInfospot = new PANOLENS.Infospot(
+//   2000,
+//   "https://images-na.ssl-images-amazon.com/images/I/91nELBuo3kL._RI_SX200_.jpg"
+// );
+// posterInfospot.position.set(-4774.9, 474.16, -1375.02);
+// panorama1.add(posterInfospot);
 
-var controlItemPoster = {
-  style: {
-    backgroundImage:
-      "url(https://images-na.ssl-images-amazon.com/images/I/31DcBWmTrNL._CR0,25,201,201.jpg)",
-    float: "left",
-  },
+// var controlItemPoster = {
+//   style: {
+//     backgroundImage:
+//       "url(https://images-na.ssl-images-amazon.com/images/I/31DcBWmTrNL._CR0,25,201,201.jpg)",
+//     float: "left",
+//   },
 
-  onTap: function () {
-    posterInfospot.focus();
-  },
-};
-viewer.appendControlItem(controlItemPoster);
+//   onTap: function () {
+//     posterInfospot.focus();
+//   },
+// };
+// viewer.appendControlItem(controlItemPoster);
 
-// Video control item
-var controlItemVideoGroup = {
-  style: {
-    backgroundImage:
-      "url(https://images-na.ssl-images-amazon.com/images/I/91ovrqFkzkL._RI_SX200_.jpg)",
-    float: "left",
-  },
+// // Video control item
+// var controlItemVideoGroup = {
+//   style: {
+//     backgroundImage:
+//       "url(https://images-na.ssl-images-amazon.com/images/I/91ovrqFkzkL._RI_SX200_.jpg)",
+//     float: "left",
+//   },
 
-  onTap: function () {
-    viewer.setPanorama(panorama);
-  },
+//   onTap: function () {
+//     viewer.setPanorama(panorama);
+//   },
 
-  group: "video",
-};
-viewer.appendControlItem(controlItemVideoGroup);
+//   group: "video",
+// };
+// viewer.appendControlItem(controlItemVideoGroup);
